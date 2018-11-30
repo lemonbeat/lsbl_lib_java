34ce29a9320b554c769367dafdd1c649ac2ef7a4
Kuffel <adam.kuffel@lemonbeat.com>
2018-11-30 13:58:31 +0100

    [mgmtb-1041] Added script to auto-generate jar files

--- src/main/java/com/lemonbeat/lsbl/lsbl_heartbeat_service/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.11.19 um 04:40:50 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:lsbl_heartbeat_service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.lemonbeat.lsbl.lsbl_heartbeat_service;
